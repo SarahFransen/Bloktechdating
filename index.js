12cddd0f2058b6027899ed346e8ccf15b17009c4
SarahFransen <sarah@therzerg.nl>
2020-05-23 16:40:43 +0200

    extra routes

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const app = express();
 const port = 8090;
 
 app.get('/', home);
+app.get('/about', about);
+app.get('/life', life);
+app.get('/plants', plants);
 
 function home(req, res) {
     res.send('hello world!');
+}
+
+function about(req, res) {
+    res.send('hello about!');
+}
+
+function life(req, res) {
+    res.send('picture will appear one day!');
+}
+
+function plants(req, res) {
+    res.send('pictures of zhe plants!');
 }
 
 app.listen(port, function() {
