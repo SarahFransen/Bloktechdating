cb8047017a9f372f143a0dd9170c7d3d8726eb59
SarahFransen <sarah@debak.nl>
2020-05-25 16:36:30 +0200

    updated stuff

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Hello reader!
 
 For this semester of college, 2019/2020 summer semester, I will begin a dating application project. At first I will work alone on one feature before joining up with others to complete it into a fully realised application. This read me has been made to explain to anyone what exactly can be found here and how to navigate my repository. 
+
+(hierneer te zetten voor developers, updaten voor hun, wiki is voor hoe het in elkaar steekt. Basics van project)
--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const app = express();
 const port = 8090;
 const err = "";
 
+app.set('view engine', 'ejs');
+app.set('views', 'view');
+
+/*routing*/
 app.get('/home', home);
 app.get('/about', about);
-app.get('/contact', contact);
+app.get('/planten', planten);
 
 app.use(function (req, res, next) {
     console.error(err.stack)
     res.status(404).send("Can't find the page you're looking for, sorry!")
 })
+
+
 
 function home(req, res) {
     res.send('hello world!');
@@ -20,9 +26,23 @@ function about(req, res) {
     res.send('hello about!');
 }
 
-function contact(req, res) {
-    res.send('Hello contact!');
+function planten(req, res) {
+    res.send('Hello planten!');
+    res.render('planten');
 }
+
+const data = [
+    {
+        name: 'raapzaad',
+        color: 'geel',
+        description: 'irritatie'
+    },
+    {
+        name: 'data1',
+        color: 'blep',
+        description: 'moet iets habben'
+    }
+]
 
 app.listen(port, function() {
     console.log('want to check the commit stuff for insurance I guess');
